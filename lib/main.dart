1f53e4db241fe2f25bc191838ca4c7014e0f8d7b
saif-ibne-ali <si87868@gmail.com>
2023-09-25 23:54:14 +0600

    modle7 assignment

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'cart_list.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,212 +8,126 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Module 6 Assignment',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Photo Gallery'),
+    return const MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      home: ProductPage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key, required this.title});
+class ProductPage extends StatefulWidget {
+  const ProductPage({super.key});
 
-  final String title;
+  @override
+  State<ProductPage> createState() => _ProductPageState();
+}
 
-  mySnackBar(message, context) {
-    return ScaffoldMessenger.of(context)
-        .showSnackBar(SnackBar(content: Text(message)));
+class _ProductPageState extends State<ProductPage> {
+  //int _counter = 0;
+
+  List<String> productName = [
+    "Dell Monitor",
+    "MSI MotherBoard",
+    "RAM 8GB",
+    "RAM 4GB",
+    "16GB Flash Drive",
+    "Keyboard",
+    "Mouse",
+    "HP Monitor",
+    "240GB SSD",
+    "500GB HDD",
+    "1TB HDD",
+    "MI TV BOX",
+    "Sony TV",
+    "MicroLab Music"
+  ];
+  List<String> itemName = [];
+  List<int> itemCount = [];
+
+  List<int> productCount = List.generate(14, (int count) => 0);
+
+  int _incrementCounter(int index) {
+    setState(() {
+      productCount[index]++;
+    });
+    //print(productCount);
+    return productCount[index];
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
+        title: const Text("Product List"),
         centerTitle: true,
-        title: Text(title),
       ),
-      body: ListView(
-        scrollDirection: Axis.vertical,
-        children: [
-          Container(
-            padding: const EdgeInsets.all(10.0),
-            child: const Text(
-              "Welcome to My Photo Gallery",
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
+      body: ListView.separated(
+        shrinkWrap: true,
+        itemCount: productName.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(productName[index]),
+            subtitle: const Text("\$20.0"),
+            trailing: Column(
+              children: [
+                Text("Counter: ${productCount[index]}"),
+                ElevatedButton(
+                  onPressed: () {
+                    int tapCount = _incrementCounter(index);
+                    if (tapCount == 5) {
+                      showDialog(
+                          barrierDismissible: false,
+                          context: context,
+                          builder: (context) {
+                            return AlertDialog(
+                              title: const Text("Congratualations!"),
+                              content:
+                                  Text("You've bought $tapCount Products!"),
+                              actions: [
+                                TextButton(
+                                    onPressed: () {
+                                      Navigator.pop(context);
+                                    },
+                                    child: const Text("OK"))
+                              ],
+                            );
+                          });
+                    }
+                  },
+                  child: const Text(
+                    "Buy Now",
+                    style: TextStyle(fontSize: 10),
+                  ),
+                )
+              ],
             ),
-          ),
-          Container(
-            padding: const EdgeInsets.all(10.0),
-            child: const TextField(
-              decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'Search for photos...'),
-            ),
-          ),
-          GridView.count(
-            padding: const EdgeInsets.all(10.0),
-            crossAxisCount: 3,
-            shrinkWrap: true,
-            children: [
-              Container(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextButton(
-                    onPressed: () {
-                      mySnackBar("Photo 0", context);
-                    },
-                    child: Column(
-                      children: [
-                        Image.network(
-                          "https://images.pexels.com/photos/2899097/pexels-photo-2899097.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-                        ),
-                        const Text(
-                          "Photo 0",
-                          style: TextStyle(color: Colors.black),
-                        )
-                      ],
-                    ),
-                  )),
-              Container(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextButton(
-                    onPressed: () {
-                      mySnackBar("Photo 1", context);
-                    },
-                    child: Column(
-                      children: [
-                        Image.network(
-                          "https://images.pexels.com/photos/2899097/pexels-photo-2899097.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-                        ),
-                        const Text(
-                          "Photo 1",
-                          style: TextStyle(color: Colors.black),
-                        )
-                      ],
-                    ),
-                  )),
-              Container(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextButton(
-                    onPressed: () {
-                      mySnackBar("Photo 2", context);
-                    },
-                    child: Column(
-                      children: [
-                        Image.network(
-                          "https://images.pexels.com/photos/2899097/pexels-photo-2899097.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-                        ),
-                        const Text(
-                          "Photo 2",
-                          style: TextStyle(color: Colors.black),
-                        )
-                      ],
-                    ),
-                  )),
-              Container(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextButton(
-                    onPressed: () {
-                      mySnackBar("Photo 3", context);
-                    },
-                    child: Column(
-                      children: [
-                        Image.network(
-                          "https://images.pexels.com/photos/2899097/pexels-photo-2899097.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-                        ),
-                        const Text(
-                          "Photo 3",
-                          style: TextStyle(color: Colors.black),
-                        )
-                      ],
-                    ),
-                  )),
-              Container(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextButton(
-                    onPressed: () {
-                      mySnackBar("Photo 4", context);
-                    },
-                    child: Column(
-                      children: [
-                        Image.network(
-                          "https://images.pexels.com/photos/2899097/pexels-photo-2899097.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-                        ),
-                        const Text(
-                          "Photo 4",
-                          style: TextStyle(color: Colors.black),
-                        )
-                      ],
-                    ),
-                  )),
-              Container(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextButton(
-                    onPressed: () {
-                      mySnackBar("Photo 5", context);
-                    },
-                    child: Column(
-                      children: [
-                        Image.network(
-                          "https://images.pexels.com/photos/2899097/pexels-photo-2899097.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-                        ),
-                        const Text(
-                          "Photo 5",
-                          style: TextStyle(color: Colors.black),
-                        )
-                      ],
-                    ),
-                  )),
-            ],
-          ),
-          const ListTile(
-            title: Text("Photo 1"),
-            subtitle: Text("Description for photo 1"),
-            leading: CircleAvatar(
-              backgroundImage: NetworkImage(
-                  "https://images.pexels.com/photos/2899097/pexels-photo-2899097.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"),
-              radius: 20,
-            ),
-          ),
-          const ListTile(
-            title: Text("Photo 2"),
-            subtitle: Text("Description for photo 2"),
-            leading: CircleAvatar(
-              backgroundImage: NetworkImage(
-                  "https://images.pexels.com/photos/2899097/pexels-photo-2899097.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"),
-              radius: 20,
-            ),
-          ),
-          const ListTile(
-            title: Text("Photo 3"),
-            subtitle: Text("Description for photo 3"),
-            leading: CircleAvatar(
-              backgroundImage: NetworkImage(
-                  "https://images.pexels.com/photos/2899097/pexels-photo-2899097.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"),
-              radius: 20,
-            ),
-          ),
-          ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              shape: const CircleBorder(),
-              padding: const EdgeInsets.all(20),
-            ),
-            onPressed: () {
-              mySnackBar("Photos Uploaded Successfully!", context);
-            },
-            child: const Icon(
-              Icons.upload,
-              color: Colors.white,
-            ),
-          )
-        ],
+          );
+        },
+        separatorBuilder: (context, index) {
+          return const Divider();
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          for (int i = 0; i < productCount.length; i++) {
+            if (productCount[i] != 0) {
+              itemName.add(productName[i]);
+              itemCount.add(productCount[i]);
+            }
+          }
+          //print(itemName);
+          //print(itemCount);
+          Navigator.push(context, MaterialPageRoute(builder: (context) {
+            return CartList(itemName: itemName, itemCount: itemCount);
+          }));
+        },
+        tooltip: 'Go To Cart',
+        child: const Icon(Icons.shopping_cart),
       ),
     );
   }
