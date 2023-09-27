03403f8c00faaa52e6f8cb32e19d5c6b74e80133
saif-ibne-ali <si87868@gmail.com>
2023-09-27 21:50:18 +0600

    WillPopScope added with proper functons to clear variables

--- lib/cart_list.dart
+++ lib/cart_list.dart
@@ -8,49 +8,56 @@ class CartList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        leading: IconButton(
-            onPressed: () {
-              Navigator.pop(context);
-              itemCount.clear();
-              itemName.clear();
-            },
-            icon: const Icon(Icons.arrow_back)),
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text("Cart"),
-        centerTitle: true,
-      ),
-      body: Column(
-        children: [
-          Container(
-            width: double.infinity,
-            padding: const EdgeInsets.all(10.0),
-            margin: const EdgeInsets.all(20.0),
-            color: Theme.of(context).colorScheme.inversePrimary,
-            child: Text(
-              "Total Products ${itemName.length}",
-              textAlign: TextAlign.center,
-            ),
-          ),
-          Expanded(
-            child: ListView.separated(
-              shrinkWrap: true,
-              itemCount: itemName.length,
-              itemBuilder: (context, index) {
-                return ListTile(
-                    title: Text(itemName[index]),
-                    subtitle: const Text("\$20.0"),
-                    trailing: Text("Count: ${itemCount[index]}"));
+    return WillPopScope(
+      child: Scaffold(
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          leading: IconButton(
+              onPressed: () {
+                Navigator.pop(context);
+                itemCount.clear();
+                itemName.clear();
               },
-              separatorBuilder: (context, index) {
-                return const Divider();
-              },
+              icon: const Icon(Icons.arrow_back)),
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: const Text("Cart"),
+          centerTitle: true,
+        ),
+        body: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              padding: const EdgeInsets.all(10.0),
+              margin: const EdgeInsets.all(20.0),
+              color: Theme.of(context).colorScheme.inversePrimary,
+              child: Text(
+                "Total Products ${itemName.length}",
+                textAlign: TextAlign.center,
+              ),
             ),
-          ),
-        ],
+            Expanded(
+              child: ListView.separated(
+                shrinkWrap: true,
+                itemCount: itemName.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                      title: Text(itemName[index]),
+                      subtitle: const Text("\$20.0"),
+                      trailing: Text("Count: ${itemCount[index]}"));
+                },
+                separatorBuilder: (context, index) {
+                  return const Divider();
+                },
+              ),
+            ),
+          ],
+        ),
       ),
+      onWillPop: () async {
+        itemCount.clear();
+        itemName.clear();
+        return true;
+      },
     );
   }
 }
