24aa855c9294abdf59668e277bda37e04148f50d
saif-ibne-ali <si87868@gmail.com>
2023-09-30 06:06:36 +0600

    module8_assignment

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# responsive_builder, device_preview and sizer
-![Alt text](image.png)
+# Create the following screen which rearranges its widgets while changing orientation.
+
+* Use any image or icon as placeholders.
+
+Portrait mode : 
+
+![Alt text](Screenshot_20230930-060310_flutter_projects.png) 
+
+Landscape mode:
+
+![Alt text](Screenshot_20230930-060322_flutter_projects.png)
--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,46 @@
-import 'package:device_preview/device_preview.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:sizer/sizer.dart';
-
-import 'home_screen.dart';
+import 'package:module6_assignment/view.dart';
 
 void main(List<String> args) {
-  runApp(DevicePreview(
-      enabled: kDebugMode,
-      builder: (context) {
-        return const MyApp();
-      }));
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
+  Widget build(Object context) {
+    return const MaterialApp(
+      title: "Module8 Assignment",
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
   Widget build(BuildContext context) {
-    return Sizer(builder: (context, orientation, deviceType) {
-      print(deviceType);
-      return MaterialApp(
-        locale: DevicePreview.locale(context),
-        builder: DevicePreview.appBuilder,
-        title: "Responsive Design",
-        home: const HomeScreen(),
-      );
-    });
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Profile"),
+      ),
+      body: OrientationBuilder(
+        builder: (context, orientation) {
+          if (orientation == Orientation.portrait) {
+            return const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: ProtraitView(),
+            );
+          } else {
+            return const Padding(
+              padding: EdgeInsets.all(5.0),
+              child: LandscapeView(),
+            );
+          }
+        },
+      ),
+    );
   }
 }
