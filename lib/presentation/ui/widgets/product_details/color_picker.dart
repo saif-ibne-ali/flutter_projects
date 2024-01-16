8fa4691bb67f6864257c8513a808af2492d0f7f0
saif-ibne-ali <si87868@gmail.com>
2024-01-16 23:45:58 +0600

    UI Design - PrductDetail Screen

---
+++ lib/presentation/ui/widgets/product_details/color_picker.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class ColorPicker extends StatefulWidget {
+  const ColorPicker({super.key, required this.colors, required this.onChange});
+
+  final List<Color> colors;
+  final Function(Color) onChange;
+
+  @override
+  State<ColorPicker> createState() => _ColorPickerState();
+}
+
+class _ColorPickerState extends State<ColorPicker> {
+  late Color _selectedColor;
+
+  @override
+  void initState() {
+    _selectedColor = widget.colors.first;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: widget.colors.map((e) => InkWell(
+        borderRadius: BorderRadius.circular(15),
+        onTap: (){
+          _selectedColor = e;
+          widget.onChange(e);
+          if(mounted){
+            setState((){});
+          }
+        },
+        child: Padding(
+          padding: const EdgeInsets.all(4.0),
+          child: CircleAvatar(
+            radius: 15,
+            backgroundColor: e,
+            child: _selectedColor == e ? const Icon(Icons.done,color: Colors.white,) : null,
+          ),
+        ),
+      )).toList()
+      ,
+    );
+  }
+}
---
+++ lib/presentation/ui/widgets/product_details/size_picker.dart
@@ -0,0 +1,53 @@
+import 'package:crafty_bay/presentation/ui/utility/app_colors.dart';
+import 'package:flutter/material.dart';
+
+class SizePicker extends StatefulWidget {
+  const SizePicker({super.key, required this.sizes, required this.onChange});
+
+  final List<String> sizes;
+  final Function(String) onChange;
+
+  @override
+  State<SizePicker> createState() => _SizePickerState();
+}
+
+class _SizePickerState extends State<SizePicker> {
+  late String _selectedSize;
+
+  @override
+  void initState() {
+    _selectedSize = widget.sizes.first;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: widget.sizes.map((e) => InkWell(
+        borderRadius: BorderRadius.circular(15),
+        onTap: (){
+          _selectedSize = e;
+          widget.onChange(e);
+          if(mounted){
+            setState((){});
+          }
+        },
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 8.0),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(100),
+            border: Border.all(
+                color: Colors.grey
+            ),
+            color: _selectedSize == e ? AppColors.primaryColor : null,
+          ),
+          child:Text(e, style: TextStyle(
+            color: _selectedSize == e ? Colors.white : Colors.black,
+          ),),
+        ),
+      )).toList()
+      ,
+    );
+  }
+}
