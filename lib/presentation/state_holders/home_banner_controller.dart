1289facfc1c1b2d4a27dbb6967981aa90f451b4a
saif-ibne-ali <si87868@gmail.com>
2024-02-04 16:04:43 +0600

    API Integration - Banner Carousel

--- lib/presentation/state_holders/home_banner_controller.dart
+++ lib/presentation/state_holders/home_banner_controller.dart
@@ -5,28 +5,31 @@ import 'package:get/get.dart';
 
 class HomeBannerController extends GetxController {
   bool _inProgress = false;
+
   bool get inProgress => _inProgress;
 
   String _errorMessage = '';
+
   String get errorMessage => _errorMessage;
 
   BannerListModel _bannerListModel = BannerListModel();
+
   BannerListModel get bannerListModel => _bannerListModel;
 
   Future<bool> getBannerList() async {
     bool isSuccess = false;
     _inProgress = true;
     update();
-    final response = await NetworkCaller().getRequest(Urls.homeBanner);
+    final response = await NetworkCaller()
+        .getRequest(Urls.homeBanner);
     _inProgress = false;
-    if (response.isSuccess && response.responseData['msg'] != 'fail') {
+    if (response.isSuccess) {
       _bannerListModel = BannerListModel.fromJson(response.responseData);
       isSuccess = true;
-      return true;
     } else {
       _errorMessage = response.errorMessage;
-      update();
-      return isSuccess;
     }
+    update();
+    return isSuccess;
   }
 }
