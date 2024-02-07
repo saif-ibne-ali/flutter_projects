8a517b5eba47a4600787768a65810b23f188164d
saif-ibne-ali <si87868@gmail.com>
2024-02-07 20:30:47 +0600

    API Integration - minor fix for user logged in issues

--- lib/presentation/state_holders/auth_controller.dart
+++ lib/presentation/state_holders/auth_controller.dart
@@ -1,20 +1,20 @@
 import 'dart:convert';
 
 import 'package:crafty_bay/data/models/profile.dart';
+import 'package:crafty_bay/presentation/ui/screens/auth/validate_email_screen.dart';
 import 'package:get/get.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class AuthController extends GetxController {
-
   static String? token;
   Profile? profile;
 
-  Future<void> saveUserDetails(String userToken, Profile userProfile) async {
+  Future<void> saveUserDetails(String t, Profile p) async {
     SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
-    await sharedPreferences.setString('token', userToken);
-    await sharedPreferences.setString('profile', jsonEncode(userProfile.toJson()));
-    token = userToken;
-    profile = userProfile;
+    await sharedPreferences.setString('token', t);
+    await sharedPreferences.setString('profile', jsonEncode(p.toJson()));
+    token = t;
+    profile = p;
   }
 
   Future<void> initialize() async {
@@ -22,7 +22,7 @@ class AuthController extends GetxController {
     profile = await _getProfile();
   }
 
-  Future<bool> isloggedIn() async {
+  Future<bool> isLoggedIn() async {
     await initialize();
     return token != null;
   }
@@ -34,18 +34,23 @@ class AuthController extends GetxController {
     return sharedPreferences.getString('token');
   }
 
-  Future<Profile?> _getProfile() async{
+  Future<Profile?> _getProfile() async {
     SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
-    final String? strProfile =  sharedPreferences.getString('profile');
-    if(strProfile==null){
+    final String? strProfile = sharedPreferences.getString('profile');
+    if (strProfile == null) {
       return null;
     } else {
       return Profile.fromJson(jsonDecode(strProfile));
     }
   }
 
-  Future<void> clearAuthData() async{
+  static Future<void> clearAuthData() async {
     SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
     await sharedPreferences.clear();
+    token = null;
+  }
+
+  static Future<void> goToLogin() async {
+    Get.to(() => const ValidateEmailScreen());
   }
 }
