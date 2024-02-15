84c3327db9b27f57216d6929f2f9b081d83721a5
saif-ibne-ali <si87868@gmail.com>
2024-02-15 20:55:07 +0600

    UI - minor bug fixes for review screen

--- lib/presentation/ui/screens/review_list_screen.dart
+++ lib/presentation/ui/screens/review_list_screen.dart
@@ -46,7 +46,7 @@ class _ReviewListScreenState extends State<ReviewListScreen> {
                   itemBuilder: (context, index) {
                     return Card(
                       child: Padding(
-                        padding: const EdgeInsets.all(8.0),
+                        padding: const EdgeInsets.all(16.0),
                         child: Column(
                           mainAxisAlignment: MainAxisAlignment.start,
                           children: [
