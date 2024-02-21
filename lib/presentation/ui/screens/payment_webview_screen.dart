8a350fbdbf73bd9879df5707222a8cad38412dcf
saif-ibne-ali <si87868@gmail.com>
2024-02-21 09:56:24 +0600

    minor bugs fix

--- lib/presentation/ui/screens/payment_webview_screen.dart
+++ lib/presentation/ui/screens/payment_webview_screen.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
@@ -11,12 +12,13 @@ class PaymentWebViewScreen extends StatefulWidget {
 }
 
 class _PaymentWebViewScreenState extends State<PaymentWebViewScreen> {
-
   late WebViewController _webViewController;
+  late String _currentUrl;
 
   @override
   void initState() {
     super.initState();
+    _currentUrl = widget.url;
     _webViewController = WebViewController()
       ..setJavaScriptMode(JavaScriptMode.unrestricted)
       ..setBackgroundColor(const Color(0x00000000))
@@ -25,8 +27,14 @@ class _PaymentWebViewScreenState extends State<PaymentWebViewScreen> {
           onProgress: (int progress) {
             // Update loading bar.
           },
-          onPageStarted: (String url) {},
-          onPageFinished: (String url) {},
+          onPageStarted: (String url) {
+            setState(() {});
+            _currentUrl = url;
+          },
+          onPageFinished: (String url) {
+            setState(() {});
+            _currentUrl = url;
+          },
           onWebResourceError: (WebResourceError error) {},
           onNavigationRequest: (NavigationRequest request) {
             if (request.url.startsWith('https://www.youtube.com/')) {
@@ -43,10 +51,39 @@ class _PaymentWebViewScreenState extends State<PaymentWebViewScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title:const Text('Payment'),
+        title: const Text('Payment'),
       ),
       body: WebViewWidget(
-        controller: _webViewController,)
+        controller: _webViewController,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Example: Close WebView and show snackbar when FAB is pressed
+          closeWebViewAndShowSnackbar();
+        },
+        child: const Icon(Icons.close),
+      ),
+    );
+  }
+
+  String extractEndpoint(String url) {
+    RegExp regex = RegExp(r'/api/(\w+)\?');
+    Match? match = regex.firstMatch(url);
+    if (match != null) {
+      return match.group(1)!;
+    } else {
+      return '';
+    }
+  }
+
+  void closeWebViewAndShowSnackbar() {
+    _webViewController.clearCache();
+    Navigator.of(context).pop();
+
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(extractEndpoint(_currentUrl)),
+      ),
     );
   }
 }
